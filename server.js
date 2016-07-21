c317e5072947764b1f4deb204272936f42828b62
Ahmed Shaaban & Hudson Burns Prestidge <pair+AhmedShab.hudson-prestidge@devbootcamp.com>
2016-07-22 11:29:22 +1200

    add routes file

--- server.js
+++ server.js
@@ -7,6 +7,18 @@ app.get('/', function(req, res) {
   res.sendFile(__dirname + '/index.html')
 })
 
+app.get('/querytest/:id', function(req, res) {
+  console.log(req.url)
+  console.log(req.params)
+  res.send(req.params.id)
+})
+
+app.get('/savetolocal', function(req, res) {
+   console.log(req.app.locals);
+   req.app.locals.adds = 10 + 2
+   console.log(req.app.locals);
+})
+
 app.listen(3000, function(req, res) {
   console.log('The server is listening on port', PORT)
 })
